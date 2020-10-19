3a5973a402c8778fb0652952dab047312c0e30c2
AMorozov-web <AMorozov-web@users.noreply.github.com>
2020-10-19 17:18:22 +0300

    Start modules creation

---
+++ js/util.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(function () {
+
+  window.util = {
+    isEscEvent: (evt, action) => {
+      if (evt.key === `Escape`) {
+        evt.preventDefault();
+        action();
+      }
+    },
+    isEnterEvent: (evt, action) => {
+      if (evt.key === `Enter`) {
+        evt.preventDefault();
+        action();
+      }
+    }
+  };
+})();
