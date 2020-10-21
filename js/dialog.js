ebf74c2ac268352d408fea059cb27e9588571a65
AMorozov-web <AMorozov-web@users.noreply.github.com>
2020-10-21 16:35:30 +0300

    Add drag function to dialog.js

--- js/dialog.js
+++ js/dialog.js
@@ -6,6 +6,7 @@
   const userSetup = document.querySelector(`.setup`);
   const setupOpen = document.querySelector(`.setup-open`);
   const setupClose = userSetup.querySelector(`.setup-close`);
+  const dialogHandle = userSetup.querySelector(`.upload`);
 
   const openPopup = () => {
     userSetup.classList.remove(`hidden`);
@@ -44,5 +45,46 @@
 
   setupClose.addEventListener(`keydown`, () => {
     closePopup();
+  });
+
+  dialogHandle.addEventListener(`mousedown`, (evt) => {
+    evt.preventDefault();
+    let startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+    let dragged = false;
+
+    const onMouseMove = (moveEvt) => {
+      moveEvt.preventDefault();
+      dragged = true;
+      let shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+      userSetup.style.top = (userSetup.offsetTop - shift.y) + `px`;
+      userSetup.style.left = (userSetup.offsetLeft - shift.x) + `px`;
+    };
+
+    const onMouseUp = (upEvt) => {
+      upEvt.preventDefault();
+      document.removeEventListener(`mousemove`, onMouseMove);
+      document.removeEventListener(`mouseup`, onMouseUp);
+
+      if (dragged) {
+        const onClickPreventDefault = (clickEvt) => {
+          clickEvt.preventDefault();
+          dialogHandle.removeEventListener(`click`, onClickPreventDefault);
+        };
+        dialogHandle.addEventListener(`click`, onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener(`mousemove`, onMouseMove);
+    document.addEventListener(`mouseup`, onMouseUp);
   });
 })();
